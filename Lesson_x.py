ab9e3071f6d5dc510697eb7a38ae9af58c7abcbe
evg85 <Evg85xxx@gmail.com>
2023-03-29 10:46:55 +0300

    creat Lesson_x

--- Lesson_x.py
+++ Lesson_x.py
@@ -1,4 +1,18 @@
-with open("jornal.txt") as file:
+from random import randint
+
+jornal = "spisok.txt"   # имя файла
+sp = []
+
+for i in range(5):
+    sp0 = input("Введите строку: ")
+    sp.append(sp0 + " оценка - ")
+    sp1 = input("Введите оценку:")
+    sp.append(sp1 + "\n")
+print(sp)
+with open(jornal, "w") as file:
+    for i in sp:
+        file.write(i)
+with open(jornal, "r") as file: # Считываем файл
     mark_count = 0
     mark_sum = 0
     print("Ученики чьи оценки меньше 3-х:")
